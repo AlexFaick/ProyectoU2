4d079e025ddc049ea125e8f51fc08e5b126149cc
ale <ale@LENOVO>
2020-11-18 21:01:36 -0600

    Proyecto de la unidad II

---
+++ src/Transformaciones/Principal.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Transformaciones;
+
+/**
+ *
+ * @author ale
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Punto figura []={
+           new Punto(20, 550),
+           new Punto(770, 550),
+           new Punto(770, 350),//altura de primer pilar
+           new Punto(517, 300),
+           new Punto(517, 290),
+           new Punto(780, 340),
+           new Punto(770, 350),
+           new Punto(780, 340),
+           new Punto(780, 325),
+           new Punto(517, 270),
+           new Punto(517, 550),
+           new Punto(517, 240),
+           new Punto(380, 80),
+           new Punto(253, 240),
+           new Punto(228, 240),
+           new Punto(380, 45),
+           new Punto(380, 45),
+           new Punto(545, 240),
+           new Punto(517, 240),
+           new Punto(380, 80),
+           new Punto(253, 240),
+           new Punto(253, 300),
+           new Punto(20, 350),
+           new Punto(10, 340),
+           new Punto(253, 285),
+           new Punto(253, 270),
+           new Punto(10, 325),
+           new Punto(10, 340),
+           new Punto(20, 350),
+           new Punto(20, 550),
+           new Punto(97, 550),
+           new Punto(97, 400),
+           new Punto(194, 400),
+           new Punto(194, 550),
+           new Punto(253, 550),
+           new Punto(253, 300),
+           new Punto(253, 550),
+           new Punto(341,550),
+           new Punto(341,400),
+           new Punto(431,400),
+           new Punto(431,550),
+           new Punto(386,550),
+           new Punto(386,400),//PUERTA DE ENMEDIO ARRIBA XD
+           new Punto(386,550),
+           new Punto(600,550),
+           new Punto(600,400),
+           new Punto(700,400),
+           new Punto(700,550),
+           
+           
+
+        
+       };
+      new Ventana("Transformaciones",figura);
+       
+       
+    }
+    
+}
